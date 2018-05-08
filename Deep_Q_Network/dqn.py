5b3b4c9fe78fb8ddf33a8f22a90d81161d51751a
BlueFisher <blue_fisher@qq.com>
2018-05-08 16:04:45 +0800

    dqn

---
+++ Deep_Q_Network/dqn.py
@@ -0,0 +1,90 @@
+import gym
+import numpy as np
+import tensorflow as tf
+from buffer import *
+
+
+initializer_helper = {
+    'kernel_initializer': tf.random_normal_initializer(0., 0.3),
+    'bias_initializer': tf.constant_initializer(0.1)
+}
+
+
+class DQN(object):
+    def __init__(self, sess, s_dim, a_dim, batch_size, gamma, lr, epsilon, replace_target_iter):
+        self.sess = sess
+        self.s_dim = s_dim  # 状态维度
+        self.a_dim = a_dim  # one hot行为维度
+        self.gamma = gamma
+        self.lr = lr # learning rate
+        self.epsilon = epsilon  # epsilon-greedy
+        self.replace_target_iter = replace_target_iter  # 经历C步后更新target参数
+
+        self.buffer = Buffer(batch_size, 10000)
+        self._learn_step_counter = 0
+        self._generate_model()
+
+    def choose_action(self, s):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.a_dim)
+        else:
+            q_eval_z = self.sess.run(self.q_eval_z, feed_dict={
+                self.s: s[np.newaxis, :]
+            })
+            return q_eval_z.squeeze().argmax()
+
+    def _generate_model(self):
+        self.s = tf.placeholder(tf.float32, shape=(None, self.s_dim), name='s')
+        self.a = tf.placeholder(tf.float32, shape=(None, self.a_dim), name='a')
+        self.r = tf.placeholder(tf.float32, shape=(None, 1), name='r')
+        self.s_ = tf.placeholder(tf.float32, shape=(None, self.s_dim), name='s_')
+        self.done = tf.placeholder(tf.float32, shape=(None, 1), name='done')
+
+        self.q_eval_z = self._build_net(self.s, 'eval_net', True)
+        self.q_target_z = self._build_net(self.s_, 'target_net', False)
+
+        # y = r + gamma * max(q^)
+        q_target = self.r + self.gamma * tf.reduce_max(self.q_target_z, axis=1, keepdims=True) * (1 - self.done)
+
+        q_eval = tf.reduce_sum(self.a * self.q_eval_z, axis=1, keepdims=True)
+        # a_mask = tf.cast(self.a, tf.bool)
+        # q_eval = tf.expand_dims(tf.boolean_mask(self.q_eval_z, a_mask), 1)
+
+        self.loss = tf.reduce_mean(tf.squared_difference(q_target, q_eval))
+        self.optimizer = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+        param_target = tf.global_variables(scope='target_net')
+        param_eval = tf.global_variables(scope='eval_net')
+
+        # 将eval网络参数复制给target网络
+        self.target_replace_ops = [tf.assign(t, e) for t, e in zip(param_target, param_eval)]
+
+    def _build_net(self, s, scope, trainable):
+        with tf.variable_scope(scope):
+            l = tf.layers.dense(s, 20, activation=tf.nn.relu, trainable=trainable, ** initializer_helper)
+            q_z = tf.layers.dense(l, self.a_dim, trainable=trainable, **initializer_helper)
+
+        return q_z
+
+    def store_transition_and_learn(self, s, a, r, s_, done):
+        if self._learn_step_counter % self.replace_target_iter == 0:
+            self.sess.run(self.target_replace_ops)
+
+        # 将行为转换为one hot形式
+        one_hot_action = np.zeros(self.a_dim)
+        one_hot_action[a] = 1
+
+        self.buffer.store_transition(s, one_hot_action, [r], s_, [done])
+        self._learn()
+        self._learn_step_counter += 1
+
+    def _learn(self):
+        s, a, r, s_, done = self.buffer.get_mini_batches()
+
+        loss, _ = self.sess.run([self.loss, self.optimizer], feed_dict={
+            self.s: s,
+            self.a: a,
+            self.r: r,
+            self.s_: s_,
+            self.done: done
+        })
---
+++ README.md
@@ -0,0 +1,1 @@
+[Deep_Q_Network](https://bluefisher.github.io/2018/05/07/Deep-Q-Network/)
